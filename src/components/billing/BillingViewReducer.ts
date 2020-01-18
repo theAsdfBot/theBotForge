011bd02f2836830d9475bd73724a1992a678e14a
Harry Chen <harrychenhc@icloud.com>
2020-01-17 23:39:02 -0500

    finished adding types, action creators, and local billing reducer

--- src/components/billing/BillingViewReducer.ts
+++ src/components/billing/BillingViewReducer.ts
@@ -1,4 +1,13 @@
-import { UserInfo, PaymentInfo } from '../types/billingTypes'
+import {
+  UserInfo,
+  PaymentInfo,
+  BillingState,
+  BillingActionType,
+  UPDATE_BILLING_DETAILS,
+  UPDATE_SHIPPING_DETAILS,
+  UPDATE_PAYMENT_DETAILS,
+  BILLING_SAME_AS_SHIPPING
+} from '../types/billingTypes'
 
 export const initialState = {
   billingSameAsShipping: false,
@@ -36,13 +45,41 @@ export const initialState = {
 }
 
 // Action Creators
-export const updateBillingDetails = (payload: UserInfo) => ({})
-export const updateShippingDetails = (payload: UserInfo) => ({})
-export const updatePaymentDetails = (payload: PaymentInfo) => ({})
+export const updateBillingDetails = (payload: UserInfo) => ({
+  type: UPDATE_BILLING_DETAILS,
+  payload
+})
+
+export const updateShippingDetails = (payload: UserInfo) => ({
+  type: UPDATE_SHIPPING_DETAILS,
+  payload
+})
+
+export const updatePaymentDetails = (payload: PaymentInfo) => ({
+  type: UPDATE_PAYMENT_DETAILS,
+  payload
+})
 
 // Reducer for BillingView
-export const billingReducer = (state: any, action: any) => {  // need to change any typing
+export const billingReducer = (state: BillingState, action: BillingActionType): BillingState => {  // need to change any typing
   switch (action.type) {
+    case UPDATE_BILLING_DETAILS:
+      return { ...state, billingDetails: action.payload }
+    case UPDATE_SHIPPING_DETAILS:
+      return { ...state, shippingDetails: action.payload }
+    case UPDATE_PAYMENT_DETAILS:
+      return { ...state, paymentDetails: action.payload }
+    case BILLING_SAME_AS_SHIPPING:
+      const previousValue = state.billingSameAsShipping
+      if (previousValue) {
+        return {
+          ...state, billingSameAsShipping: false, shippingDetails: {
+            ...initialState.shippingDetails
+          }
+        }
+      } {
+        return { ...state, billingSameAsShipping: true, shippingDetails: { ...state.billingDetails } }
+      }
     default:
       return state
   }
--- src/components/types/billingTypes.ts
+++ src/components/types/billingTypes.ts
@@ -1,6 +1,7 @@
 export const UPDATE_BILLING_DETAILS = 'UPDATE_BILLING_DETAILS'
 export const UPDATE_SHIPPING_DETAILS = 'UPDATE_SHIPPING_DETAILS'
 export const UPDATE_PAYMENT_DETAILS = 'UPDATE_PAYMENT_DETAILS'
+export const BILLING_SAME_AS_SHIPPING = 'BILLING_SAME_AS_SHIPPING'
 
 export interface UserInfo {
   firstName: string,
@@ -24,4 +25,32 @@ export interface PaymentInfo {
   profileName: string
 }
 
-export type BillingActionType = UPDATE_BILLING_DETAILS | UPDATE_SHIPPING_DETAILS | UPDATE_PAYMENT_DETAILS
+// initial state type
+export interface BillingState {
+  billingSameAsShipping: boolean,
+  billingDetails: UserInfo,
+  shippingDetails: UserInfo,
+  paymentDetails: PaymentInfo
+}
+
+// types for action
+export interface UpdateBillingDetailsAction {
+  type: typeof UPDATE_BILLING_DETAILS,
+  payload: UserInfo
+}
+
+export interface UpdateShippingDetailsAction {
+  type: typeof UPDATE_SHIPPING_DETAILS,
+  payload: UserInfo
+}
+
+export interface BillingDetailsSameAsShipping {
+  type: typeof BILLING_SAME_AS_SHIPPING,
+}
+
+export interface UpdatePaymentDetailsAction {
+  type: typeof UPDATE_PAYMENT_DETAILS,
+  payload: PaymentInfo
+}
+
+export type BillingActionType = UpdateBillingDetailsAction | UpdateShippingDetailsAction | UpdatePaymentDetailsAction | BillingDetailsSameAsShipping

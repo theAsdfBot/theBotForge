7bbac5323f93b9b50f82e53ea32ffc6fbc9ee7f6
Harry Chen <harrychenhc@icloud.com>
2020-06-17 01:00:12 -0400

    add functionality for the edit button. pressing it will enable the profile to be editable. it defaults to lock mode

--- src/components/billing/details/PaymentDetails.tsx
+++ src/components/billing/details/PaymentDetails.tsx
@@ -5,14 +5,16 @@ import { StoreAction } from '../store/types'
 import { UPDATE_PAYMENT_KEY } from '../store/actions'
 
 import FormInput from '../../common/FormInput'
+import Overlay from '../../common/Overlay'
 
 type PaymentDetailsProps = {
   paymentDetails: PaymentInfo,
   errors: PaymentInfo,
+  editable: boolean,
   dispatch: React.Dispatch<StoreAction>
 }
 
-const PaymentDetails: FunctionComponent<PaymentDetailsProps> = ({ paymentDetails, dispatch, errors }) => {
+const PaymentDetails: FunctionComponent<PaymentDetailsProps> = ({ paymentDetails, editable, dispatch, errors }) => {
   const { nameOnCard, cardNumber, expirationMonth, expirationYear, securityCode, email, profileName } = paymentDetails
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => dispatch({
@@ -26,17 +28,18 @@ const PaymentDetails: FunctionComponent<PaymentDetailsProps> = ({ paymentDetails
       <div className='bg-gray-850 text-center text-xl w-2/3 mb-2'>
         <h4 className='text-white'>Payment Details</h4>
       </div>
+      {!editable ? <Overlay styles={{ height: '450px' }} classes='w-80' /> : null}}
       <div className='flex flex-col'>
         {/*TODO: ENUM here for card type*/}
         <FormInput type='text' name='cardNumber' placeholder='Credit Card Number' value={cardNumber} onChange={onChange} maxLength={19} error={errors.cardNumber} classes={'my-1'} />
         <div className='my-1 flex justify-center'>
-          <FormInput type='text' name='expirationMonth' placeholder='Month' value={expirationMonth} onChange={onChange} error={errors.expirationMonth} maxLength={4} size={5} classes={'flex-grow'} />
-          <FormInput type='text' name='expirationYear' placeholder='Year' value={expirationYear} onChange={onChange} error={errors.expirationYear} maxLength={4} size={5} classes={'mx-1 flex-grow'} />
-          <FormInput type='text' name='securityCode' placeholder='CVV' value={securityCode} onChange={onChange} error={errors.securityCode} maxLength={4} size={5} classes={'flex-grow'} />
+          <FormInput type='text' name='expirationMonth' placeholder='Month' value={expirationMonth} onChange={onChange} error={errors.expirationMonth} maxLength={4} size={5} classes={'flex-grow'} disabled={!editable} />
+          <FormInput type='text' name='expirationYear' placeholder='Year' value={expirationYear} onChange={onChange} error={errors.expirationYear} maxLength={4} size={5} classes={'mx-1 flex-grow'} disabled={!editable} />
+          <FormInput type='text' name='securityCode' placeholder='CVV' value={securityCode} onChange={onChange} error={errors.securityCode} maxLength={4} size={5} classes={'flex-grow'} disabled={!editable} />
         </div>
-        <FormInput type='text' name='nameOnCard' placeholder='Full Name' value={nameOnCard} onChange={onChange} error={errors.nameOnCard} classes={'my-1'} />
-        <FormInput type='text' name='email' placeholder='Email' value={email} onChange={onChange} error={errors.email} classes={'my-1'} />
-        <FormInput type='text' name='profileName' placeholder='Profile Name' value={profileName} onChange={onChange} error={errors.profileName} classes={'my-1'} />
+        <FormInput type='text' name='nameOnCard' placeholder='Full Name' value={nameOnCard} onChange={onChange} error={errors.nameOnCard} classes={'my-1'} disabled={!editable} />
+        <FormInput type='text' name='email' placeholder='Email' value={email} onChange={onChange} error={errors.email} classes={'my-1'} disabled={!editable} />
+        <FormInput type='text' name='profileName' placeholder='Profile Name' value={profileName} onChange={onChange} error={errors.profileName} classes={'my-1'} disabled={!editable} />
       </div>
     </div>
   )

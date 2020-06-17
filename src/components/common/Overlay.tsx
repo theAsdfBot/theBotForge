86d1a7cc42aa2540d06098f468f4accd076eb2d8
Harry Chen <harrychenhc@icloud.com>
2020-06-17 00:40:27 -0400

    made overlay more flexible instead of hardcored height and width

--- src/components/common/Overlay.tsx
+++ src/components/common/Overlay.tsx
@@ -1,7 +1,17 @@
 import React, { FunctionComponent } from 'react'
 
-const Overlay: FunctionComponent = () => {
-  return <div className='overlay'></div>
+type OverlayProps = {
+  classes?: string,
+  styles?: any // style objects in case we want to hard card a specific height, etc.
+}
+
+const Overlay: FunctionComponent<OverlayProps> = ({ styles, classes }) => {
+  return <div className={`overlay ${classes}`} style={styles} ></div >
+}
+
+Overlay.defaultProps = {
+  styles: {},
+  classes: ''
 }
 
 export default Overlay

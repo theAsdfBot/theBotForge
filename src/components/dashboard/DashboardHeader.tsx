8a595ae5b853aa86578cdad7eba963de54bbb7c6
Harry Chen <harrychenhc@icloud.com>
2020-07-10 00:10:23 -0400

    refactor DashbaordHeader to be more DRY

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,15 +1,22 @@
 import React, { FunctionComponent } from 'react'
 
+const tableHeaders = [
+  'TASK',
+  'WEBSITE',
+  'KEYWORDS/LINKS',
+  'SIZE',
+  'PROFILE',
+  'SIZE',
+  'PROXY',
+  'STATUS'
+]
+
 const DashboardHeader: FunctionComponent = () => {
   return (
     <div className='bg-gray-850 h-12 flex justify-start mb-2'>
-      <span className="ml-20 mr-10 py-3">TASK</span>
-      <span className="mx-10 py-3" >WEBSITE</span>
-      <span className="mx-10 py-3">KEYWORDS/LINKS</span>
-      <span className="mx-10 py-3">SIZE</span>
-      <span className="mx-10 py-3">PROFILE</span>
-      <span className="mx-10 py-3">PROXY</span>
-      <span className="mx-10 py-3">STATUS</span>
+      {tableHeaders.map((title, idx) => {
+        return <span className={`py-3 ${idx > 0 ? 'mx-10' : 'ml-20 mr-10'}`}>{title}</span>
+      })}
     </div>
   )
 }
